be30942fb938c88bc54622212525c5b2f4c34acf
Joshua Madrigal <madrigaljosh10@yahoo.com>
2024-07-19 14:14:56 -0500

    Initial commit with project

---
+++ InProgress/InProgress/InProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  InProgressApp.swift
+//  InProgress
+//
+//  Created by Joshua Madrigal on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct InProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
